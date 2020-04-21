e149121bf9db88d56762014e2f5657ddca6e85e2
maciejambroziak@gmail.com <maciejambroziak@gmail.com>
2020-04-21 21:45:05 +0200

    html for login register, main page done some server side work done (it is possible to render templates) almost no sql work done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 import data_manager
 app = Flask(__name__)
 
@@ -7,6 +7,31 @@ app = Flask(__name__)
 def index():
     services = data_manager.get_all()
     return render_template('Table.html', services=services)
+
+@app.route('/dodaj_wydarzenie', methods = ['GET', 'POST'])
+def add_event(): 
+    if request.method == 'POST':
+        request.form['']
+        return redirect('index')
+    else:
+        return render_template('event_questionare.html')
+
+@app.route('/zarejestruj', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        request.form['']
+        return redirect('index')
+    else:
+        return render_template('register.html')
+
+
+@app.route('/zaloguj', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return redirect('index')
+    else: 
+        return render_template('login.html')
+
 
 @app.route('/trenerzy_osobisci')
 def trenerzy_osobisci():
