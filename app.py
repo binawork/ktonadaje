b69e383f5f3acfaf87a289e2c8d379e13d97dfd8
kMatejak <ksmatejak@gmail.com>
2020-05-21 20:35:41 +0200

    created add-event form without adding to database

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-import os
+
 import config
+from forms import EventForm
 
 
 app = Flask(__name__)
@@ -13,52 +15,63 @@ from models import *
 
 
 @app.route('/')
+@app.route('/events')
 def index():
     events = Event.query.all()
-    return render_template('Table.html',
-                           events=events)
+    return render_template(
+                        'events_table.html', 
+                        events=events
+                        )
 
 
-@app.route('/dodaj_wydarzenie', methods = ['GET', 'POST'])
+@app.route('/add-event', methods = ('GET', 'POST'))
 def add_event():
-    if request.method == 'POST':
-        request.form['']
-        return redirect('index')
-    else:
-        return render_template('event_questionare.html')
+    form = EventForm()
+    choices = [
+        (category.title, category.title) for category in Category.query.order_by("title")
+    ]
+    print(choices)
+    form.event_category.choices = choices
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+    
+    return render_template('events/add_event.html', form=form)
 
 
-@app.route('/zarejestruj', methods = ['GET', 'POST'])
+
+
+
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
     if request.method == 'POST':
         request.form['']
         return redirect('index')
     else:
-        return render_template('register.html')
+        return render_template('users/register.html')
 
 
-@app.route('/zaloguj', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         return redirect('index')
     else:
-        return render_template('login.html')
+        return render_template('users/login.html')
 
 
-@app.route('/trenerzy_osobisci')
+@app.route('/services')
 def services_catalog():
     services = "usługi"
     return render_template('services_catalog.html', services=services)
 
 
-@app.route('/o_nas')
-def about_us():
-    return render_template('about_us.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
-@app.route('/jak_streamowac')
-def how_to_stream():
-    return render_template('how_to_stream.html')
+@app.route('/how-to-streaming')
+def howto_streaming():
+    return render_template('howto_streaming.html')
 
 
 if __name__ == '__main__':
